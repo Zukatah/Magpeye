538fde5e86dd418a46ef5256ca0d0522ae02c666
Zukatah <andreasbeicht@gmail.com>
2024-05-24 21:25:27 +0200

    learn.ps1 restructuring nothing important

--- createNewModel.py
+++ createNewModel.py
@@ -3,50 +3,55 @@ from keras.models import Model
 from keras.layers import Input, Dense, Conv3D, MaxPooling3D, BatchNormalizationV2, Flatten, Dropout, ZeroPadding3D
 from globalConstants import TRAINING_EXAMPLE_DEPTH, TRAINING_EXAMPLE_HEIGHT, TRAINING_EXAMPLE_WIDTH
 
-modelPath = "Models/model_10x240x135_zp011_10c233111_bn_mp143143_zp011_10c233111_bn_zp011_10c233111_bn_mp121121_zp011_10c233111_bn_zp011_10c233111_bn_mp123123_zp011_10c233111_bn_zp011_10c233111_bn_mp133133_fl_den8_do10"
+modelPath = "Models/model_10x240x135_zp011_6c233_bn_mp123_zp011_6c233_bn_zp011_6c233_bn_mp121_zp011_6c233_bn_zp011_6c233_bn_zp000001_mp122_zp011_6c233_bn_zp011_6c233_bn_zp000001_mp122_zp011_6c233_bn_zp011_6c233_bn_mp133_fl_den80_do50_2"
 
 
 print("Creating model...")
 input_layer = Input(shape=(TRAINING_EXAMPLE_DEPTH,TRAINING_EXAMPLE_HEIGHT,TRAINING_EXAMPLE_WIDTH,1), name='input_layer')
 
 ZeroPadding3D_1 = ZeroPadding3D(padding=(0,1,1))(input_layer)
-Conv3D_1 = Conv3D(10, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_1)
+Conv3D_1 = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_1)
 BaNo_1 = BatchNormalizationV2()(Conv3D_1)
-MaxPool3D_1 = MaxPooling3D(pool_size=(1,4,3), strides=(1,4,3), padding="valid")(BaNo_1)
+MaxPool3D_1 = MaxPooling3D(pool_size=(1,2,3), strides=(1,2,3), padding="valid")(BaNo_1)
 
 ZeroPadding3D_2a = ZeroPadding3D(padding=(0,1,1))(MaxPool3D_1)
-Conv3D_2a = Conv3D(10, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_2a)
+Conv3D_2a = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_2a)
 BaNo_2a = BatchNormalizationV2()(Conv3D_2a)
 ZeroPadding3D_2b = ZeroPadding3D(padding=(0,1,1))(BaNo_2a)
-Conv3D_2b = Conv3D(10, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_2b)
+Conv3D_2b = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_2b)
 BaNo_2b = BatchNormalizationV2()(Conv3D_2b)
 MaxPool3D_2 = MaxPooling3D(pool_size=(1,2,1), strides=(1,2,1), padding="valid")(BaNo_2b)
 
 ZeroPadding3D_3a = ZeroPadding3D(padding=(0,1,1))(MaxPool3D_2)
-Conv3D_3a = Conv3D(10, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_3a)
+Conv3D_3a = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_3a)
 BaNo_3a = BatchNormalizationV2()(Conv3D_3a)
 ZeroPadding3D_3b = ZeroPadding3D(padding=(0,1,1))(BaNo_3a)
-Conv3D_3b = Conv3D(10, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_3b)
+Conv3D_3b = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_3b)
 BaNo_3b = BatchNormalizationV2()(Conv3D_3b)
-MaxPool3D_3 = MaxPooling3D(pool_size=(1,2,3), strides=(1,2,3), padding="valid")(BaNo_3b)
+ZeroPadding3D_3c = ZeroPadding3D(padding=((0,0),(0,0),(0,1)))(BaNo_3b)
+MaxPool3D_3 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), padding="valid")(ZeroPadding3D_3c)
 
 ZeroPadding3D_4a = ZeroPadding3D(padding=(0,1,1))(MaxPool3D_3)
-Conv3D_4a = Conv3D(10, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_4a)
+Conv3D_4a = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_4a)
 BaNo_4a = BatchNormalizationV2()(Conv3D_4a)
 ZeroPadding3D_4b = ZeroPadding3D(padding=(0,1,1))(BaNo_4a)
-Conv3D_4b = Conv3D(10, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_4b)
+Conv3D_4b = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_4b)
 BaNo_4b = BatchNormalizationV2()(Conv3D_4b)
-MaxPool3D_4 = MaxPooling3D(pool_size=(1,3,3), strides=(1,3,3), padding="valid")(BaNo_4b)
+ZeroPadding3D_4c = ZeroPadding3D(padding=((0,0),(0,0),(0,1)))(BaNo_4b)
+MaxPool3D_4 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), padding="valid")(ZeroPadding3D_4c)
+
+ZeroPadding3D_5a = ZeroPadding3D(padding=(0,1,1))(MaxPool3D_4)
+Conv3D_5a = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_5a)
+BaNo_5a = BatchNormalizationV2()(Conv3D_5a)
+ZeroPadding3D_5b = ZeroPadding3D(padding=(0,1,1))(BaNo_5a)
+Conv3D_5b = Conv3D(6, (2,3,3), strides=(1,1,1), activation="relu", padding="valid")(ZeroPadding3D_5b)
+BaNo_5b = BatchNormalizationV2()(Conv3D_5b)
+MaxPool3D_4 = MaxPooling3D(pool_size=(1,3,3), strides=(1,3,3), padding="valid")(BaNo_5b)
 
 Flatten_1 = Flatten()(MaxPool3D_4)
-Dense_1 = Dense(units=8, activation="relu")(Flatten_1)
-Dropout_1 = Dropout(0.1)(Dense_1)
+Dense_1 = Dense(units=80, activation="relu")(Flatten_1)
+Dropout_1 = Dropout(0.5)(Dense_1)
 output_layer = Dense(units=4, activation="softmax")(Dropout_1)
-
-
-
-
-
 
 model = Model(inputs=input_layer, outputs=output_layer)
 
--- learn.ps1
+++ learn.ps1
@@ -1,55 +1,46 @@
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_mp121121_zp011_6c233111_bn_zp011_6c233111_bn_mp123123_zp011_6c233111_bn_zp011_6c233111_bn_mp133133_fl_den24_do30" --learningRate 0.001 --epochs 25
+<# CM, 4xCCM 10=>1 #>
 
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_8c233111_bn_mp143143_zp011_8c233111_bn_zp011_8c233111_bn_mp121121_zp011_8c233111_bn_zp011_8c233111_bn_mp123123_zp011_8c233111_bn_zp011_8c233111_bn_mp133133_fl_den16_do20" --learningRate 0.001 --epochs 15
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233_bn_mp123_zp011_6c233_bn_zp011_6c233_bn_mp121_zp011_6c233_bn_zp011_6c233_bn_zp000001_mp122_zp011_6c233_bn_zp011_6c233_bn_zp000001_mp122_zp011_6c233_bn_zp011_6c233_bn_mp133_fl_den80_do50_2" --learningRate 0.001 --epochs 12
 
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_10c233111_bn_mp143143_zp011_10c233111_bn_zp011_10c233111_bn_mp121121_zp011_10c233111_bn_zp011_10c233111_bn_mp123123_zp011_10c233111_bn_zp011_10c233111_bn_mp133133_fl_den8_do10" --learningRate 0.001 --epochs 25
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233_bn_mp123_zp011_6c233_bn_zp011_6c233_bn_mp121_zp011_6c233_bn_zp011_6c233_bn_zp000001_mp122_zp011_6c233_bn_zp011_6c233_bn_zp000001_mp122_zp011_6c233_bn_zp011_6c233_bn_mp133_fl_den80_do50" --learningRate 0.001 --epochs 12
 
 
-<# 3xCCM 10=>4 #>
+
+<# CM, CCCM, CCCM 10=>3 #>
 <#
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp133133_fl_den32_do40" --learningRate 0.001 --epochs 15
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp155155_fl_den96_do50" --learningRate 0.001 --epochs 15
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_fl_den48_do50" --learningRate 0.001 --epochs 10
+
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_8c233111_bn_mp143143_zp011_8c233111_bn_zp011_8c233111_bn_zp011_8c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_fl_den48_do50" --learningRate 0.001 --epochs 10
 #>
 
 
-<# 3xCCM 10=>1 #>
+<# CM, 3xCCM 10=>3 #>
 <#
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c333111_bn_mp143143_zp011_4c233111_bn_zp011_4c333111_bn_mp143143_zp011_4c233111_bn_zp011_4c333111_bn_mp133133_fl_den128_do50" --learningRate 0.001 --epochs 8
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c333111_bn_zp011_4c233111_bn_mp143143_zp011_4c333111_bn_zp011_4c233111_bn_mp143143_zp011_4c333111_bn_zp011_4c233111_bn_mp133133_fl_den128_do50" --learningRate 0.001 --epochs 8
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_mp121121_zp011_8c233111_bn_zp011_8c233111_bn_mp123123_zp011_10c233111_bn_zp011_10c233111_bn_mp133133_fl_den12_do20" --learningRate 0.001 --epochs 10
+
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_mp121121_zp011_6c233111_bn_zp011_6c233111_bn_mp123123_zp011_6c233111_bn_zp011_6c233111_bn_mp133133_fl_den24_do30" --learningRate 0.001 --epochs 10
+
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_8c233111_bn_mp143143_zp011_8c233111_bn_zp011_8c233111_bn_mp121121_zp011_8c233111_bn_zp011_8c233111_bn_mp123123_zp011_8c233111_bn_zp011_8c233111_bn_mp133133_fl_den16_do20" --learningRate 0.001 --epochs 10
+
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_10c233111_bn_mp143143_zp011_10c233111_bn_zp011_10c233111_bn_mp121121_zp011_10c233111_bn_zp011_10c233111_bn_mp123123_zp011_10c233111_bn_zp011_10c233111_bn_mp133133_fl_den8_do10" --learningRate 0.001 --epochs 10
 #>
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233111_bn_mp123123_zp011_6c233111_bn_zp011_6c233111_bn_mp121121_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_mp155155_fl_den64_do50" --learningRate 0.001 --epochs 12
 
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233111_bn_mp123123_zp011_6c233111_bn_zp011_6c233111_bn_mp121121_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_mp133133_fl_den24_do30" --learningRate 0.001 --epochs 12
 
-<# 4xCCM 10=>2 #>
-<#
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_mp121121_zp011_6c233111_bn_zp011_6c233111_bn_mp123123_zp011_6c233111_bn_zp011_6c233111_bn_mp133133_fl_den48_do40" --learningRate 0.001 --epochs 15
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp121121_zp011_4c233111_bn_zp011_4c233111_bn_mp123123_zp011_4c233111_bn_zp011_4c233111_bn_mp133133_fl_den64_do50" --learningRate 0.001 --epochs 15
-#>
-
-
-<# 3xCCCM 10=>1 #>
-<#
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233111_bn_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_10c232111_bn_10c232111_bn_10c232111_bn_mp122122_fl_den48_do40" --learningRate 0.001 --epochs 9
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_6c233111_bn_6c233111_bn_6c233111_bn_mp133133_8c232111_bn_8c232111_bn_8c232111_bn_mp122122_fl_den64_do50" --learningRate 0.001 --epochs 9
-#>
 
 
 <# CM, CCM, CCCM 10=>4 #>
 <#
 python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_8c233111_bn_mp143143_zp011_8c233111_bn_zp011_8c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_fl_den32_do40" --learningRate 0.001 --epochs 15
+
 python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_6c233111_bn_6c233111_bn_6c233111_bn_mp133133_fl_den48_do50" --learningRate 0.001 --epochs 15
+
 python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_4c233111_bn_4c233111_bn_4c233111_bn_mp133133_fl_den96_do50" --learningRate 0.001 --epochs 15
+
 python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_fl_den48_do50" --learningRate 0.001 --epochs 15
+
 python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_zp011_8c233111_bn_zp011_8c233111_bn_mp143143_12c233111_bn_12c233111_bn_12c233111_bn_mp133133_fl_den24_do30" --learningRate 0.001 --epochs 15
-#>
-
-
-<# CM, CCCM, CCCM 10=>3 #>
-<#
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_8c233111_bn_mp143143_zp011_8c233111_bn_zp011_8c233111_bn_zp011_8c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_fl_den48_do50" --learningRate 0.001 --epochs 25
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_zp011_6c233111_bn_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_fl_den48_do50" --learningRate 0.001 --epochs 15
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_6c233111_bn_6c233111_bn_6c233111_bn_mp133133_8c232111_bn_8c232111_bn_8c232111_bn_mp122122_fl_den16_do20" --learningRate 0.001 --epochs 15
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_12c232111_bn_12c232111_bn_12c232111_bn_mp122122_fl_den8_do10" --learningRate 0.001 --epochs 15
-python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_mp143143_6c244111_bn_6c244111_bn_6c244111_bn_mp133133_8c244111_bn_8c244111_bn_8c244111_bn_mp143143_fl_den128_do50" --learningRate 0.001 --epochs 15
 #>
 
 
@@ -60,6 +51,38 @@ python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_12c233111_bn_zp0
 python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_12c233111_bn_zp011_12c233111_bn_mp143143_zp011_12c233111_bn_zp011_12c233111_bn_mp143143_12c233111_bn_12c233111_bn_12c233111_bn_mp133133_fl_den8_do10" --learningRate 0.001 --epochs 4
 #>
 
+
+<# 3xCCCM 10=>1 #>
+<#
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_6c233111_bn_zp011_6c233111_bn_zp011_6c233111_bn_mp143143_8c233111_bn_8c233111_bn_8c233111_bn_mp133133_10c232111_bn_10c232111_bn_10c232111_bn_mp122122_fl_den48_do40" --learningRate 0.001 --epochs 9
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_6c233111_bn_6c233111_bn_6c233111_bn_mp133133_8c232111_bn_8c232111_bn_8c232111_bn_mp122122_fl_den64_do50" --learningRate 0.001 --epochs 9
+#>
+
+
+<# 4xCCM 10=>2 #>
+<#
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp121121_zp011_4c233111_bn_zp011_4c233111_bn_mp123123_zp011_4c233111_bn_zp011_4c233111_bn_mp133133_fl_den64_do50_2" --learningRate 0.001 --epochs 5
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp121121_zp011_4c233111_bn_zp011_4c233111_bn_mp123123_zp011_4c233111_bn_zp011_4c233111_bn_mp133133_fl_den64_do50_3" --learningRate 0.001 --epochs 5
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp121121_zp011_4c233111_bn_zp011_4c233111_bn_mp123123_zp011_4c233111_bn_zp011_4c233111_bn_mp133133_fl_den96_do50" --learningRate 0.001 --epochs 5
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp121121_zp011_4c233111_bn_zp011_4c233111_bn_mp123123_zp011_4c233111_bn_zp011_4c233111_bn_mp133133_fl_den48_do50" --learningRate 0.001 --epochs 5
+#>
+
+
+<# 3xCCM 10=>1 #>
+<#
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c333111_bn_mp143143_zp011_4c233111_bn_zp011_4c333111_bn_mp143143_zp011_4c233111_bn_zp011_4c333111_bn_mp133133_fl_den128_do50" --learningRate 0.001 --epochs 6
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c333111_bn_zp011_4c233111_bn_mp143143_zp011_4c333111_bn_zp011_4c233111_bn_mp143143_zp011_4c333111_bn_zp011_4c233111_bn_mp133133_fl_den128_do50" --learningRate 0.001 --epochs 6
+#>
+
+
+<# 3xCCM 10=>4 #>
+<#
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp133133_fl_den32_do40" --learningRate 0.001 --epochs 6
+python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp143143_zp011_4c233111_bn_zp011_4c233111_bn_mp155155_fl_den96_do50" --learningRate 0.001 --epochs 6
+#>
+
+
+
 <# CM, CCM, CCCM 10=>4? #>
 <#
 python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_12c233111_bn_mp143143_zp011_12c233111_bn_zp011_12c233111_bn_mp143143_12c233111_bn_12c233111_bn_12c233111_bn_mp133133_fl_den8_do10" --learningRate 0.001 --epochs 2
@@ -69,46 +92,4 @@ python loadAndTrainModel.py --modelPath "model_10x240x135_zp011_12c233111_bn_mp1
 <# --evaluate #>
 <#
 python loadAndTrainModel.py --modelPath "cp_0_1482_model_10x240x135_zp011_12c233111_bn_zp011_12c233111_bn_mp143143_zp011_12c233111_bn_zp011_12c233111_bn_mp143143_zp011_12c233111_bn_zp011_12c233111_bn_mp155155_fl_den16_do20" --learningRate 0.001 --epochs 1 --evaluate
-#>
-
-
-<#
-sora ai videos
-
-Sensitivity-Slider
-val 0 => img 366 9783 9733
-
-testen, ob trainingsexmaples exakt gespeichert werden (bytes statt ints)
-#>
-
-<#
-
-(1) 0.1875: cM   CCM  CCM    => 3 3 16 => 8(10)
-(2) 0.1960: cCM  CCM  CCM    => 3 3 16 => 8(10)
-(3) 0.1962: CcCM CCCM        => 3 3 16 => 8(10)
-(3) 0.2025: cCM  CCM  CCM    => 3 3 16 => 8(10)		255
-(4) 0.2098: CcM  CCM  CCM    => 3 3 16 => 8(10)
-(5) 0.2287: cCM  CCM         => 3 3 16 => 8(10)
-(6) 0.2482: cM   CCM  CCM    => 3 3 16 => 4(0)
-(7) 0.2493: CM   CCM  CCM CM => 1 1 36 => 16(20)
-
-3xCCM
-1xCM, 2xCCM
-1xCM, 2xCCM, 1xCM
-3xCM
-2xCCM, 1xCC
-2xCCCM
-4xCC bzw. 3xCC, 1xC NoMP
-
-3xCCM 255
-1xCM, 2xCCM 255
-2xCCCM 255
-
-3xCCM 10=>4
-3xCCM 10=>1
-4xCCM 10=>2
-3xCCCM 10=>1
-2xCCM, 1xCCCM 10=>3
-CM, CCM, CCCM 10=>3
-
 #>
